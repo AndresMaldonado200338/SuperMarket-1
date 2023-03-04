f5bb0ca4756ef9f448a18ec024428295ae4e5f07
Andres Maldonado <andres.maldonado02@uptc.edu.co>
2023-03-03 22:37:18 -0500

    V1.3

--- src/model/SuperMarket.java
+++ src/model/SuperMarket.java
@@ -1,6 +1,12 @@
 package model;
+
+import java.io.FileWriter;
 public class SuperMarket {
 
+    private static final String SUPPPLIER_PATH = "/src/resources/suppliers.txt";
+    private static final String PRODUCT_PATH = "/src/resources/products.txt";
+    private static final String CUSTOMER_PATH = "/src/resources/customers.txt";
+    private static final String CATEGORIES_PATH = "/src/resources/categories.txt";
 
     public void createProduct(Product product){
         
